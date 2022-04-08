3c73a7686aec70082ed291c02a73ae42c08a91db
yemicoder <dubiafx@gmail.com>
2022-04-07 21:35:14 -0700

    oya

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/* main - prints  a set of provided strings
+/*
+ * main - prints  a set of provided strings
  *
  * Return: 0
  */
 
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
