2bd2a72e48dc76013b34a3b14571aaf0e645d8dc
yemicoder <dubiafx@gmail.com>
2022-04-08 03:54:44 -0700

    oya na

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,11 @@
 
 /**
  * main - entry point
- * i: first operand
- * d: second operand
- * c: third operand
- * f: fourth operand
+ * c: first operand
+ * i: second operand
+ * l: third operand
+ * p: fourth operand
+ * f: fifth operand
  *
  * Return: 0
  *
@@ -15,15 +16,17 @@ int main(void)
 
 {
 
-int i;
-double d;
 char c;
+int i;
+long l;
+long long p;
 float f;
 
-printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
-printf("size of a double: %lu.\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu.\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu\n", (unsigned long)sizeof(i));
+printf("size of a long int: %lu\n", (unsigned long)sizeof(l));
+printf("size of a long long int: %lu\n", (unsigned long)sizeof(p));
+printf("size of a float: %lu\n", (unsigned long)sizeof(f));
 
 return (0);
 
