1ef255a47f1c57d58381a08dc9c9e0748798fe4b
yemicoder <dubiafx@gmail.com>
2022-04-08 15:34:42 -0700

    ok

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -20,7 +20,7 @@ int main(void)
 	while (alphabet <= 'z')
 	putchar(alphabet);
 	alphabet++;
-	alphabet = 'a'
+	alphabet = 'a';
 	while (alphabet <= 'z')
 	alphabet = toupper(alphabet);
 	putchar(alphabet);
