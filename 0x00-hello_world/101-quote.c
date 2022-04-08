7bde90a0ab0b24e0b8c5142278216b82b590db96
yemicoder <dubiafx@gmail.com>
2022-04-08 05:04:38 -0700

    emi gan baddest boy

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1
+ *
+ */
+
+int main(void)
+
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+
+}
