0c68a27a358613a933c26760e91054a5a55bf19c
yemicoder <dubiafx@gmail.com>
2022-04-08 03:37:16 -0700

    oya na

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -22,8 +22,8 @@ float f;
 
 printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
-printf("size of a double: %lu./", (unsigned long)sizeof(d));
-printf("size of a float: %lu./", (unsigned long)sizeof(f));
+printf("size of a double: %lu.\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu.\n", (unsigned long)sizeof(f));
 
 return (0);
 
