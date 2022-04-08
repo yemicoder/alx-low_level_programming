622711fef4f258ea76e3577f441d9bbda83f7198
yemicoder <dubiafx@gmail.com>
2022-04-08 13:35:19 -0700

    mo commit e

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * alphabet: first variable
+ *
+ * Rerutn:0
+ *
+ */
+
+int main(void)
+{	char alphabet;
+
+	alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		printf("%c ", alphabet);
+		alphabet++;
+	}
+	return (0);
+}
