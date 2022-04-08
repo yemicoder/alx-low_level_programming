bd5936798e607fd68e5df7237df032919a172038
yemicoder <dubiafx@gmail.com>
2022-04-08 16:39:39 -0700

    task 5 done and dusted

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * n: first variable
+ *
+ * Return:0
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+		putchar('\n');
+		return (0);
+}
