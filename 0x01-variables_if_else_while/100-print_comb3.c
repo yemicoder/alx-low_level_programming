5353065b60a55f5368f2e2e05d0e64ca6efc40a2
yemicoder <dubiafx@gmail.com>
2022-04-08 18:59:10 -0700

    lol

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,14 +19,16 @@ int main(void)
 	int c;
 
 	n = 0;
+	x = 1;
 	while (n <= 9)
 	{
-	x = n + 1 + '0';
+	x = n + 1;
+	b = n + 1 + '0';
 	c = n + '0';
 	while (x <= 9)
 	{
 	putchar(c);
-	putchar(x);
+	putchar(b);
 	putchar(',');
 	putchar(' ');
 	x++;
