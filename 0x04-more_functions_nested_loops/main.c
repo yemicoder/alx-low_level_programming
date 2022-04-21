0ca7f9d86dbf8018b55e3fd83b3ec54c01d5a812
yemicoder <dubiafx@gmail.com>
2022-04-21 09:22:31 -0700

    fucking correct;

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * c: character variable
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
