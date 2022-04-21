31776e2df79d1aaf5e1dc517619f8ea83981106f
yemicoder <dubiafx@gmail.com>
2022-04-21 09:53:38 -0700

    second commit for the day

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _isdigit - check for digits
+ *
+ * @c: variable to check
+ *
+ * Return:0
+ */
+
+int _isdigit(inc c)
+{
+	if (c >= 0 && c <= 9)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
