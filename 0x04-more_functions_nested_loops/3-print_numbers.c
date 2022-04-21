dcb8ea3f40316a157c3587c66aea0dc8f693f677
yemicoder <dubiafx@gmail.com>
2022-04-21 12:19:02 -0700

    ok

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * print_numbers - this function prints numbers 0 - 9
+ *
+ * @i: variable i
+ * @c: variable c
+ * Return: 0
+ */
+
 void print_numbers(void)
 {
+	int i;
+	char c;
+
 	for (i = 0, i <= 0, i++)
 	{
-	_putchar('i');
+	c = i;
+	_putchar(c);
+	_putchar('\n');
 	}
 }
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -13,9 +13,24 @@ int mul(int a, int b)
         return (a * b);
 }
 
+void print_numbers(void)
+{
+	int i = 0;
+	char c;
+
+        while (i <= 9)
+        {
+	c = i;
+        putchar(c);
+	i++;
+	putchar('\n');
+        }
+}
+
 int main(void)
 {
 	printf("%d\n", mul(98, 1024));
 	printf("%d\n", mul(-402, 4096));
+	print_numbers();
 	return (0);
 }
