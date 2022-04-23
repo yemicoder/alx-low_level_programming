7a5f36bc26d35089cd4fdffe678fb71efd2a533f
yemicoder <dubiafx@gmail.com>
2022-04-23 14:34:50 -0700

    okkkk

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,6 +12,7 @@
 void print_diagonal(int n)
 {
 	int x;
+	int y;
 
 	for (x = 1; x <= n; x++)
 	{
@@ -25,11 +26,11 @@ void print_diagonal(int n)
 			{
 				_putchar('\\');
 			}
+		}
 		_putchar('\n');
 		}
 		if (n <= 0)
 		{
 			_putchar('\n');
 		}
-}
 }
