000a95ec4aca52eaa0a69c8f3d3aab17e754777f
yemicoder <dubiafx@gmail.com>
2022-04-23 13:23:00 -0700

    on the right track

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print \ in diagonal
+ *
+ * @x : variable
+ * @y : variable
+ * @n : variable
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+
+	for (x = 1; x <= n; x++)
+	{
+		for (y = 1; y <= n; y++)
+		{
+			if (x = y)
+			{
+				_putchar('\');
+			}
+		}
+		_putchar('\n');
+	}
+}
