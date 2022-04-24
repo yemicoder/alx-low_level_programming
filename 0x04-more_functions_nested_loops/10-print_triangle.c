796c779d20182abe69df0f2f2ba304d815215fdb
yemicoder <dubiafx@gmail.com>
2022-04-24 07:39:30 -0700

    God is good

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - function to print # in trangale shape
+ *
+ * @i : variable i
+ * @j : variable j
+ * @size : variable size
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i  <= size; i++)
+	{
+		for (j = size; j <= size; j--)
+		{
+			_putchar('#');
+		}
+		if (size <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
