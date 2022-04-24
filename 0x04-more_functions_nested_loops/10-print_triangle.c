184a8ea0bfa12c30b39e5411fee7cb00c5d907c2
yemicoder <dubiafx@gmail.com>
2022-04-24 07:51:24 -0700

    God dey

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,12 +10,15 @@
 
 void print_triangle(int size)
 {
-	int i;
 	int j;
 
-	for (i = 1; i  <= size; i++)
+	for (int i = 1; i  <= size; i++)
 	{
-		for (j = 1; j <= size; j--)
+		for (j = 1; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
 		}
